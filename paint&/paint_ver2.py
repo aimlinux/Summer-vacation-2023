29470b251f72b4032722017013f6a6fe23ac174c
Kohara <1k8aioon@gmail.com>
2023-08-26 13:31:40 +0900

    Update paint_ver2.py

--- paint&/paint_ver2.py
+++ paint&/paint_ver2.py
@@ -30,7 +30,7 @@ class BackgroundFrame(tk.Frame):
     def __init__(self, master=None, bg_image=None, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         
-        bg_image_path = "C:/Users/1k8ai/Documents/GitHub/Typing-Game/paint&/image/aimlinux.png"
+        bg_image_path = "paint&/image/aimlinux.png"
         
         if bg_image:
             self.bg_image = PhotoImage(file=bg_image_path)
