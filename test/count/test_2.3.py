410b85fbc0bbf2fef10896005b17f641ac6c47cf
Kohara <1k8aioon@gmail.com>
2023-09-04 13:27:53 +0900

    Update test_2.3.py

--- test/count/test_2.3.py
+++ test/count/test_2.3.py
@@ -1,13 +1,10 @@
 import tkinter as tk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import numpy as np
 import matplotlib.animation as animation
-import time
-from time import sleep
 
 root = tk.Tk()
-root.geometry("500x500")
+root.geometry("600x300")
 
 # MatplotlibのFigureを作成
 fig = Figure(figsize=(6, 6), dpi=100)
@@ -16,13 +13,16 @@ ax.set_aspect('equal')
 ax.axis('off')
 
 # 初期フォントサイズ
-initial_font_size = 200
+initial_font_size = 180
 
 # アニメーションの初期化
 font_size = initial_font_size
+font_name = "Yu Gothic"
+font_weight = "bold"
+font = (font_name, font_size, font_weight)
 num_text = 5
 
-t1 = ax.text(0.5, 0.5, str(num_text), ha='center', va='center', fontsize=font_size)
+t1 = ax.text(0.5, 0.5, str(num_text), ha='center', va='center', fontdict={'fontname' : font_name, 'fontsize' : font_size, 'fontweight' : font_weight})
 
 def init():
     return t1,
@@ -69,23 +69,21 @@ def update(num):
             
     elif num_text == 1:
         if font_size == 0:
-            num_text = 0
+            num_text = "スタート"
             font_size = initial_font_size
         else:
             font_size = font_size - 2
             t1.set_fontsize(font_size)
             t1.set_text(str(num_text))
-            
-    elif num_text == 0:
+        
+    elif num_text == "スタート":
         if font_size == 0:
-            #カウント終了
-            time.sleep(0.4)
-            root.destroy()
+            # カウント終了
+            root.after(400, root.destroy)  # 400ミリ秒後にウィンドウを閉じる
         else:
             font_size = font_size - 2
             t1.set_fontsize(font_size)
             t1.set_text(str(num_text))
-        
         
     return t1,
 
@@ -94,6 +92,6 @@ canvas = FigureCanvasTkAgg(fig, master=root)
 canvas_widget = canvas.get_tk_widget()
 canvas_widget.pack()
 
-ani = animation.FuncAnimation(fig, update, init_func=init, interval=7, frames=10)  # 8フレームで1秒ごとのズームイン・ズームアウト
+ani = animation.FuncAnimation(fig, update, init_func=init, interval=7, frames=10)
 
 root.mainloop()
